9726050ab7f47d8d978af56dcac20b285f169a60
Omm Prakash Jena <ojena643@gmail.com>
2026-07-19 17:17:12 +0530

    sss

--- backend/app/auth/jwt.py
+++ backend/app/auth/jwt.py
@@ -2,15 +2,15 @@ from datetime import datetime, timedelta, timezone
 
 from jose import jwt
 
-SECRET_KEY = "CHANGE_THIS_TO_ENV_LATER"
+from app.core.config import settings
 
-ALGORITHM = "HS256"
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 
 def create_access_token(data: dict):
-
     to_encode = data.copy()
 
     expire = datetime.now(timezone.utc) + timedelta(
